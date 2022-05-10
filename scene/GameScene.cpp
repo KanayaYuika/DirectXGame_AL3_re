3c60268162724ab147971f0bc23ad9de9c20ed31
YuikaKanaya <k021g1290@g.neec.ac.jp>
2022-05-10 15:17:31 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,103 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include"AxisIndicator.h"
+#include"PrimitiveDrawer.h"
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() { delete model_; delete debugCamera_; }
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager:: Load("mario.jpg");
+	//3Dモデルの生成
+	model_ = Model::Create(); //ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+	//軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する（アドレス渡し）
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+
+	for (int i = 0; i < 30; i++) {
+		if (i < 15) {
+			iti[i] = Vector3(-14+i*2, 0, -14);
+			ni[i] = Vector3(-14+i*2, 0, 14);
+		}
+		else {
+			iti[i] = Vector3(-14, 0, -14+(i-15)*2);
+			ni[i] = Vector3(14, 0, -14+(i-15)*2);
+		}
+	}
+}
+
+
+void GameScene::Update() {
+	debugCamera_->Update();
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	/// 3Dモデル描画
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+
+	//ライン描画が参照するビュープロジェクションを指定する（アドレス渡し）
+	for (int i = 0; i < 30; i++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(iti[i], ni[i], {0,0,250,1});
+	}
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
