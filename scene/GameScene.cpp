30b42f8e624d357f29e2b903267840e6a289eabf
YuikaKanaya <k021g1290@g.neec.ac.jp>
2022-05-17 15:23:46 +0900

    re:02_02.ワールド変換行列

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -8,6 +8,8 @@ GameScene::GameScene() {}
 
 GameScene::~GameScene() { delete model_; delete debugCamera_; }
 
+float PI = 3.141592;
+
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
@@ -40,6 +42,63 @@ void GameScene::Initialize() {
 			ni[i] = Vector3(14, 0, -14+(i-15)*2);
 		}
 	}
+
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = { 5.0f,5.0f,5.0f };
+	//スケーリング行列を宣言
+	Matrix4 matScale;
+	matScale = MathUtility::Matrix4Identity();//スケーリング倍率を行列に設定する
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+
+	//X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = { PI / 4,PI / 4,0 };
+	//合成用回転行列を宣言
+	Matrix4 matRot = MathUtility::Matrix4Identity();
+	//Z軸回転行列を宣言
+	Matrix4 matRotZ = MathUtility::Matrix4Identity();
+	Matrix4 matRotX = MathUtility::Matrix4Identity();
+	Matrix4 matRotY = MathUtility::Matrix4Identity();
+
+	matRotZ.m[0][0] = cos(worldTransform_.rotation_.z);
+	matRotZ.m[0][1] = sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][0] = -sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][1] = cos(worldTransform_.rotation_.z);
+
+	matRotX.m[1][1] = cos(worldTransform_.rotation_.x);
+	matRotX.m[1][2] = sin(worldTransform_.rotation_.x);
+	matRotX.m[2][1] = -sin(worldTransform_.rotation_.x);
+	matRotX.m[2][2] = cos(worldTransform_.rotation_.x);
+
+	matRotY.m[0][0] = cos(worldTransform_.rotation_.y);
+	matRotY.m[0][2] = -sin(worldTransform_.rotation_.y);
+	matRotY.m[2][0] = sin(worldTransform_.rotation_.y);
+	matRotY.m[2][2] = cos(worldTransform_.rotation_.y);
+
+	matRot *= matRotZ;
+	matRot *= matRotX;
+	matRot *= matRotY;
+
+
+	//X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = { 10,10,10 };
+	//平行移動行列を宣言
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+
+	matTrans.m[3][0] = worldTransform_.translation_.x;
+	matTrans.m[3][1] = worldTransform_.translation_.y;
+	matTrans.m[3][2] = worldTransform_.translation_.z;
+
+
+	worldTransform_.matWorld_ = MathUtility::Matrix4Identity();
+	worldTransform_.matWorld_ *= matScale;
+	worldTransform_.matWorld_ *= matRot;
+	worldTransform_.matWorld_ *= matTrans;
+
+	//行列の転送
+	worldTransform_.TransferMatrix();
+
 }
 
 void GameScene::Update() {
